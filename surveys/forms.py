7ee4c238b82daea1649dd6851583a3e09e4a998d
titus <tiephone@gmail.com>
2020-08-06 21:41:26 +0100

    ModelForm constructor

--- surveys/forms.py
+++ surveys/forms.py
@@ -1,31 +1,35 @@
 from django import forms
+from .models import MentorCall
+#class MentorCall(forms.Form):
+#
+#    learners_name = forms.CharField(
+#            label = 'Learners Name',
+#            widget = forms.TextInput
+#            )
+#    learners_email = forms.EmailField(
+#            label='Learner\'s Email', 
+#            widget = forms.EmailInput
+#            )
+#    mentors_name = forms.CharField(
+#            label = 'Mentors Name',
+#            widget = forms.TextInput
+#            )
+#    mentors_email = forms.EmailField(
+#            label='Mentor\'s Email',
+#            widget = forms.EmailInput
+#            )
+#    subject = forms.CharField(
+#            label='What did you want to discuss in this call?',
+#            widget=forms.Textarea
+#            )
+#    response = forms.CharField(
+#            label='Mentor\'s Response',
+#            widget=forms.Textarea
+#            )
+class MentorCallForm(forms.ModelForm):
+    class Meta:
+        model = MentorCall
 
-class MentorCall(forms.Form):
-
-    learners_name = forms.CharField(
-            label = 'Learners Name',
-            widget = forms.TextInput
-            )
-    learners_email = forms.EmailField(
-            label='Learner\'s Email', 
-            widget = forms.EmailInput
-            )
-    mentors_name = forms.CharField(
-            label = 'Mentors Name',
-            widget = forms.TextInput
-            )
-    mentors_email = forms.EmailField(
-            label='Mentor\'s Email',
-            widget = forms.EmailInput
-            )
-    subject = forms.CharField(
-            label='What did you want to discuss in this call?',
-            widget=forms.Textarea
-            )
-    response = forms.CharField(
-            label='Mentor\'s Response',
-            widget=forms.Textarea
-            )
 
 
         
--- surveys/models.py
+++ surveys/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 # Create your models here.
-
+class MentorCall(models.Model):
+    learner_name = models.CharField()
+    learner_email = models.CharField()
+    mentor_name = models.CharField()
+    mentor_email = models.CharField()
+    subject = models.CharField
+    response = models.CharField()
